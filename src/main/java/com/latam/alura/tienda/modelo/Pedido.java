5dd3d3b214f837596ab0fbeeb397adc8e267e078
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-05-25 23:36:35 -0500

    commit 13: Comienza Curso JAVA+JPA Avanzado

---
+++ src/main/java/com/latam/alura/tienda/modelo/Pedido.java
@@ -0,0 +1,72 @@
+package com.latam.alura.tienda.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+@Table(name="pedidos")
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private LocalDate fecha;
+	private BigDecimal valorTotal;
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	
+	public Pedido() {
+		
+	}
+	
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+
+
+}
